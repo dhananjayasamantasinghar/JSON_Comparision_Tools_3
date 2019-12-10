0979fccec00e65d0bdf42b32b5200d06f46c757c
dhananjayasamantasinghar <52064780+dhananjayasamantasinghar@users.noreply.github.com>
2019-12-10 20:25:33 +0530

    bug fix

--- Comparision-Utils/src/main/java/com/ct/utils/CommpoUtils.java
+++ Comparision-Utils/src/main/java/com/ct/utils/CommpoUtils.java
@@ -12,6 +12,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CommpoUtils {
 
+	private static Map<String, Object> objectFactory = new HashMap<>();
+
 	private static void doSimpleMapping(Object podEmployee, Object poa_Employee, Field field, String mappingValue) {
 		try {
 			Method[] declaredMethods = poa_Employee.getClass().getDeclaredMethods();
@@ -30,17 +32,19 @@ public class CommpoUtils {
 			throws InstantiationException, IllegalAccessException, ClassNotFoundException, NoSuchMethodException,
 			SecurityException, IllegalArgumentException, InvocationTargetException {
 		String[] values = mappingValue.split("/");
-		Map<String, Object> objectFactory = new HashMap<>();
+
 		for (int i = 0; i < values.length; i++) {
 			String value = values[i];
 			if (i == 0) {
-				//create a objectFactory and Store the object
+				// create a objectFactory and Store the object
 				setFirstComplexType(podEmployee, poa_Employee, field, values, value, objectFactory);
 			} else if (i > 0 && i != values.length - 1) {
-				//Get the previous object from Object factory and set current object with previous
+				// Get the previous object from Object factory and set current
+				// object with previous
 				setComplexTypeExceptFirst(values, i, value, objectFactory);
 			} else {
-				//Get the previous object from Object factory and set variable value in that
+				// Get the previous object from Object factory and set variable
+				// value in that
 				setVariableData(podEmployee, field, values, i, value, objectFactory);
 			}
 		}
@@ -68,18 +72,21 @@ public class CommpoUtils {
 
 		for (Field field2 : declaredFields1) {
 			Method[] declaredMethods = obj2.getClass().getDeclaredMethods();
-			String[] split = field2.toGenericString().split(" ");
-			String className = split[1];
-			String newValue = value;
-			if (className.contains(value)) {
-				newValue = field2.getName();
-			}
-			for (Method method : declaredMethods) {
-				if (method.getName().equalsIgnoreCase("set" + newValue)) {
-					Object obj = Class.forName(className).newInstance();
-					method.invoke(obj2, obj);
-					objectFactory.put(values[i - 1], obj2);
-					objectFactory.put(value, obj);
+
+			if (field2.getName().contains(value) || field2.getName().equalsIgnoreCase(value)) {
+				String[] split = field2.toGenericString().split(" ");
+				String className = split[1];
+				String newValue = value;
+				if (className.contains(value)) {
+					newValue = field2.getName();
+				}
+				for (Method method : declaredMethods) {
+					if (method.getName().equalsIgnoreCase("set" + newValue)) {
+						Object obj = Class.forName(className).newInstance();
+						method.invoke(obj2, obj);
+						objectFactory.put(values[i - 1], obj2);
+						objectFactory.put(value, obj);
+					}
 				}
 			}
 		}
@@ -88,28 +95,31 @@ public class CommpoUtils {
 	private static void setFirstComplexType(Object podEmployee, Object poa_Employee, Field field, String[] values,
 			String value, Map<String, Object> objectFactory)
 			throws InstantiationException, IllegalAccessException, ClassNotFoundException, InvocationTargetException {
-		Field[] declaredFields = poa_Employee.getClass().getDeclaredFields();
-		for (Field field2 : declaredFields) {
-			String[] split = field2.toGenericString().split(" ");
-			String className = split[1];
-			String newValue = value;
-			if (className.contains(value)) {
-				newValue = field2.getName();
-			}
-			if (field2.getName().equalsIgnoreCase(newValue)) {
-				Object obj = Class.forName(className).newInstance();
-				for (Method method : poa_Employee.getClass().getDeclaredMethods()) {
-					if (method.getName().equalsIgnoreCase("set" + newValue)) {
-						method.invoke(poa_Employee, obj);
-						objectFactory.put(value, obj);
-						break;
+		if (objectFactory.isEmpty() || (!objectFactory.isEmpty() && objectFactory.get(value) != null)) {
+			Field[] declaredFields = poa_Employee.getClass().getDeclaredFields();
+			for (Field field2 : declaredFields) {
+				String[] split = field2.toGenericString().split(" ");
+				String className = split[1];
+				String newValue = value;
+				if (className.contains(value)) {
+					newValue = field2.getName();
+				}
+				if ((field2.getName().contains(newValue) || field2.getName().equalsIgnoreCase(newValue))
+						&& objectFactory.get(value) == null) {
+					Object obj = Class.forName(className).newInstance();
+					for (Method method : poa_Employee.getClass().getDeclaredMethods()) {
+						if (method.getName().equalsIgnoreCase("set" + newValue)) {
+							method.invoke(poa_Employee, obj);
+							objectFactory.put(value, obj);
+							break;
+						}
 					}
 				}
 			}
 		}
 	}
 
-	public static <T> T createObjectAsPerMaping(Object podEmployee, Class<T>  clazz) {
+	public static <T> T createObjectAsPerMaping(Object podEmployee, Class<T> clazz) {
 		try {
 			return getPodEmployee(podEmployee, clazz);
 		} catch (Exception e) {
